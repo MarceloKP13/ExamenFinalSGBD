98ba5fadc15d87d0be612fcc220d2b2c0030c7a4
Marcelo Torres <kevin.torres85@outlook.es>
2025-02-25 21:35:11 -0500

    i

--- php/iniciarsesion.php
+++ php/iniciarsesion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inciar Sesión</title>
+    <title>Iniciar Sesión</title>
     <link rel="stylesheet" href="../anexos/css/login.css">
 </head>
 <body>
